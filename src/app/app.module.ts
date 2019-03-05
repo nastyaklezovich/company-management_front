3c1845fff8cdc16fc1e17f80b297d277cc903033
nastyaklezovich <nastyaklezovich@gmail.com>
2019-03-05 23:47:24 +0300

    commit-authorization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import{ ReactiveFormsModule} from '@angular/forms';
+import{ ReactiveFormsModule, FormsModule} from '@angular/forms';
 import{ HttpClientModule} from '@angular/common/http'
 import{ OrderService } from './order.service';
 import {RouterModule} from '@angular/router'
@@ -45,6 +45,7 @@ const routes = [
 
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
